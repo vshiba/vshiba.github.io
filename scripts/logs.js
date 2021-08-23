1af0740e158cce3a23d907ac6637d76c4e1de538
rosie <89108711+rosielol@users.noreply.github.com>
2021-08-23 16:49:51 +0200

    Update logs.js

--- scripts/logs.js
+++ scripts/logs.js
@@ -1,17 +1,12 @@
 function mouseOver() {
   document.getElementById("hover1").style.color = "#000000";
   document.getElementById("hover1").style.backgroundColor = "#00FF1A";
-}
-function mouseOut() {
-  document.getElementById("hover1").style.color = "#00FF1A";
-  document.getElementById("hover1").style.backgroundColor = "#000000";
-}
-
-function mouseOver() {
   document.getElementById("hover2").style.color = "#000000";
   document.getElementById("hover2").style.backgroundColor = "#00FF1A";
 }
 function mouseOut() {
+  document.getElementById("hover1").style.color = "#00FF1A";
+  document.getElementById("hover1").style.backgroundColor = "#000000";
   document.getElementById("hover2").style.color = "#00FF1A";
   document.getElementById("hover2").style.backgroundColor = "#000000";
 }
