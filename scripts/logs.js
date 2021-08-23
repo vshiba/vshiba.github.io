0b280aea24782db5511bcb20717751453421857d
rosie <89108711+rosielol@users.noreply.github.com>
2021-08-23 16:36:09 +0200

    Update logs.js

--- scripts/logs.js
+++ scripts/logs.js
@@ -1,1 +1,9 @@
+function mouseOver() {
+  document.getElementById("hover").style.color = "#000000";
+  document.getElementById("hover").style.backgroundColor = "#00FF1A";
+}
 
+function mouseOut() {
+  document.getElementById("demo").style.color = "#00FF1A";
+  document.getElementById("hover").style.backgroundColor = "#000000";
+}
